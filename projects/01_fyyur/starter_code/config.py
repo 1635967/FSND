54a29952c86ab51752dc2d0f48eb5e7db4871a83
PC <azzozzd@gmail.com>
2020-08-02 21:29:37 +0300

    config.py

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +8,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+con = psycopg2.connect(user = "PC", password = "admin", host = "127.0.0.1", port = "5432", database = "demo")
+cur = con.cursor()
+#
+# rows = cur.execute("SELECT id FROM table2")
+#
+# for r in rows:
+#     print(f"id {r[0]}")
+#
+# cur.close()
+# con.close()
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = '127.0.0.1'        #'<Put your local database url>'
